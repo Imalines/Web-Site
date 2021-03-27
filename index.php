e64aebe36f190cda8a0a94bfe9e27adbf241b8a9
Imalines <59148408+Imalines@users.noreply.github.com>
2021-03-27 14:25:01 +0500

    Добавление главное страницы сайта

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta content="">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+     <link rel="stylesheet" type="text/css" href="main.css">
+    <title>Список лабораторных работ Исаев И. С.</title>
+  </head>
+  <body>
+    <ul id="nav">
+      <li><a href="WWW/lab1/" class="link">Лабораторная работа №1</a>
+      <li><a href="WWW/lab2/" class="link">Лабораторная работа №2</a>
+      <li><a href="WWW/lab3/" class="link">Лабораторная работа №3</a>
+      <li><a href="WWW/lab4/" class="link">Лабораторная работа №4</a>
+      <li><a href="WWW/lab5/" class="link">Лабораторная работа №5</a>
+    </ul>
+
+  </body>
+</html>
